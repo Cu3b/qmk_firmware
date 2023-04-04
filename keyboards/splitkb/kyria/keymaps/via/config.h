0fc223408ec4a80c7fd6d5878dc0c3ad4ca4fd2a
Cu3b <89937459+Cu3b@users.noreply.github.com>
2023-04-05 00:46:17 +0300

    keymap added for kyria

--- keyboards/splitkb/kyria/keymaps/via/config.h
+++ keyboards/splitkb/kyria/keymaps/via/config.h
@@ -36,3 +36,7 @@
 #    define RGBLIGHT_VAL_STEP 8
 #    define RGBLIGHT_LIMIT_VAL 150
 #endif
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
